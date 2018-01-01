5679e360b6d54fec5a88dff6dd49ac938302741f
William E. Seyler <wseyler@pentaho.com>
2017-12-31 20:57:33 -0500

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :locations do
+  	collection do
+  	  get 'last'
+  	end
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
